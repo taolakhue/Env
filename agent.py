991cb07717ddb155fd3707c65644c0d8a6b8660f
taolakhue <taolakhue63@gmail.com>
2021-11-09 18:45:40 -0500

    Add API keys files

---
+++ agent.py
@@ -0,0 +1,9 @@
+class tradingAgent():
+    def __init__(self,env,config=None):
+        pass
+
+
+# reward function
+reward = self.net_worth - prev_net_worth
+# actions (0= buy, 1= sell, 2= buy)
+#
--- get_market_data.py
+++ get_market_data.py
@@ -1,10 +1,41 @@
 from dotenv import load_dotenv
 import os
+import cbpro
 
 
-API_key = os.getenv("API_KEYS")
-load_dotenv() #load .env files
+class Authenticate():
+    
+    def __init__(self, API_key, code, API_phrase):
+        self.API_key = API_key
+        self.code = code
+        self.API_phrase = API_phrase
+    
+    
+    def Client_Auth(self):
+        url = "https://api-public.sandbox.pro.coinbase.com"
+        #Authentication
+        auth_client = cbpro.AuthenticatedClient(self.API_key, self.code, self.API_phrase,
+                                  api_url=url)
+    
+class myWebSocketClient(cbpro.WebsocketClient):
+    def on_open(self):
+    
+    def on_message(self):
+    
+    def 
 
 
 
+# Paramters are optional
+wsClient = cbpro.WebsocketClient(url="wss://ws-feed.pro.coinbase.com", products="BTC-USD")
+# Do other stuff...
 
+
+#Testing
+# Get API keys in env files 
+API_key = os.getenv("API_key")
+b64secret = os.getenv("code")
+passphrase = os.getenv("API_phrase")
+load_dotenv()
+
+#m = Authenticate()
