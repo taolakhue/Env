081ca2676fb172eaa35d28cd5487a965d3870eeb
taolakhue <taolakhue63@gmail.com>
2021-11-09 19:24:34 -0500

    Authentication

--- get_market_data.py
+++ get_market_data.py
@@ -9,26 +9,17 @@ class Authenticate():
         self.API_key = API_key
         self.code = code
         self.API_phrase = API_phrase
-    
-    
+
     def Client_Auth(self):
         url = "https://api-public.sandbox.pro.coinbase.com"
         #Authentication
         auth_client = cbpro.AuthenticatedClient(self.API_key, self.code, self.API_phrase,
                                   api_url=url)
+        return auth_client
     
-class myWebSocketClient(cbpro.WebsocketClient):
-    def on_open(self):
-    
-    def on_message(self):
-    
-    def 
-
-
-
-# Paramters are optional
-wsClient = cbpro.WebsocketClient(url="wss://ws-feed.pro.coinbase.com", products="BTC-USD")
-# Do other stuff...
+    def deposit(self):
+        
+        
 
 
 #Testing
@@ -37,5 +28,9 @@ API_key = os.getenv("API_key")
 b64secret = os.getenv("code")
 passphrase = os.getenv("API_phrase")
 load_dotenv()
+
+#creating a bot object
+m= Authenticate(API_key, b64secret, passphrase)
+m.Client_Auth()
 
 #m = Authenticate()
